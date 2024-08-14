56847399a449a32d21202ff252f9aa0abd56e2a6
Elenche <elena.naumoska96@gmail.com>
2024-08-14 17:01:42 +0200

    Added Android and iOS platform

---
+++ android/app/src/main/java/com/skoop/ionicweave/MainActivity.java
@@ -0,0 +1,5 @@
+package com.skoop.ionicweave;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.skoop.ionicweave',
+  appName: 'ionic-weave',
+  webDir: 'www'
+};
+
+export default config;
